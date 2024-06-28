e1878275c052cffd97bdbaa94810a6dbc632d0b7
daunita <markpustog@gmail.com>
2024-06-28 21:19:42 +0500

    initial

---
+++ common/src/main/java/com/akicater/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.akicater;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "itemplacer";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
