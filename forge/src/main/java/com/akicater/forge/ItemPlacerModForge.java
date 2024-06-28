5e29b0ac74a721dc3a5c399c223342186764bc8e
daunita <markpustog@gmail.com>
2024-06-28 21:33:20 +0500

    renamed

--- forge/src/main/java/com/akicater/forge/ItemPlacerModForge.java
+++ forge/src/main/java/com/akicater/forge/ItemPlacerModForge.java
@@ -2,21 +2,12 @@ package com.akicater.forge;
 
 import com.akicater.ItemPlacer;
 import dev.architectury.platform.forge.EventBuses;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import java.util.function.Consumer;
-
 @Mod(ItemPlacer.MODID)
-public final class ExampleModForge {
-    public ExampleModForge() {
+public final class ItemPlacerModForge {
+    public ItemPlacerModForge() {
         // Submit our event bus to let Architectury API register our content on the right time.
         EventBuses.registerModEventBus(ItemPlacer.MODID, FMLJavaModLoadingContext.get().getModEventBus());
 
